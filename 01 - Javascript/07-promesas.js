edf2cb66ab33d341c1122b86ddebbe3354a2bc5d
Theo96sc <theo_96.sc@outlook.es>
2019-11-18 16:59:40 -0500

    La CA trabajando en el master

--- 01 - Javascript/07-promesas.js
+++ 01 - Javascript/07-promesas.js
@@ -1,5 +1,5 @@
 /*************************************************** 07-promesas****/
-
+//File Systemx
 const fs = require('fs');
 
 const sumarDosNumeros = (correcto) => {
@@ -53,8 +53,7 @@ const leerArchivo = (pathArchivo)=>{
     );
 };
 
-
-
+/*
 const promesaLecturaArch = leerArchivo('./05-callBacks.js');
 
 promesaLecturaArch
@@ -79,9 +78,50 @@ promesaLecturaArch
             console.log('Error',error);
         }
     );
+*/
+
+console.log('Inicia Sincrono');
+const nombreArchivo = './05-callbacks.js';
+//AL USAR SINCRONO SIEMPRE SE DEBE TENER TRY CATCH
+try{
+    const contenidoArchivo= fs.readFileSync(nombreArchivo);
+    console.log(contenidoArchivo);
+    console.log('Se leyo sincronamente');
+
+}catch(e){
+    console.error('Error: ',error);
+}
+console.log('Termina Sincrona');
 
 
+//Transformar promesa a codigo SINCRONO
+//Funciones normales o funciones anonimas
 
+//1) ASYNC nos permite usar codigo sincrono dentro de una funcion
+//2) Para transformar PROMESA -> SINCRONO
+// "await"
 
+async function leerArchivoSync(){
+    try{
+        const contenido= await leerArchivo(nombreArchivo);  //PROmesa
+        console.log(contenido);
+        console.log('LEIMOS CON ASYNC AWAIT');
+        return 1;
+    }catch (error){
+        console.eror ('Error ', error);
+        return 0;
+    }
+}
 
+//TODA FUNCION QUE TENGA ASYNC SE TRANSFORMA EN UNA PROMESA
 
+leerArchivoSync.then(
+    (numero)=>{
+        console.log(numero);    //1
+
+    }
+).catch(
+    (numero)=>{
+        console.log(numero);    //0
+    }
+);//PROMESA
