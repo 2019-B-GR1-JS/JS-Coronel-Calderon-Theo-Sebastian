c4d9d9db8cac06f7d4fe2c65182584443156eea8
Theo96sc <theo_96.sc@outlook.es>
2019-11-25 18:05:30 -0500

    Componentes part 2

--- 03-angular/componentes/src/app/app.component.ts
+++ 03-angular/componentes/src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'componentes';
+
+  arregloObjetos = [
+    {
+      id:1,               //number
+      "nombre":"Theo",    //string
+      "apellido":'Coronel',    //string
+      "sueldo":'100.00',    //number
+      "casado":false,    //boolean
+      comida: 'https://assets3.thrillist.com/v1/image/2797371/size/tmg-article_default_mobile.jpg',
+      carro:null,
+      ojos:undefined,
+    },
+    {
+      id:2,               //number
+      "nombre":"Juan",    //string
+      "apellido":'Perez',    //string
+      "sueldo":'500.00',    //number
+      "casado":false,    //boolean
+      comida: 'https://www.educationquizzes.com/library/KS1-Science/Food-from-plants-main.jpg',
+      carro:null,
+      ojos:undefined,
+    },
+    {
+      id:3,               //number
+      "nombre":"Conny",    //string
+      "apellido":'Benitez',    //string
+      "sueldo":'1000.00',    //number
+      "casado":false,    //boolean
+      comida: 'https://www.wbcsd.org/var/site/storage/images/media/images/fresh_pa/80809-2-eng-GB/FRESH_PA_i1140.jpg',
+      carro:null,
+      ojos:undefined,
+    },
+
+  ];
+
+  imprimirSueldo(sueldo:string){
+    console.log(sueldo);
+  }
 }
--- 03-angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-angular/componentes/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,19 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.scss']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+//  class (etiqueta html) (web-component)
+//  Instanciar a la  clase
+//  Clase esta lista
+//  Clase se destruye
 
-  ngOnInit() {
+  @Input()
+  public titulo:string;
+
+  @Input()
+  public imagen:string;
+
+  @Input()
+  public textoImagen:string;
+
+  @Input()
+  public valueW:number=200;
+  @Input()
+  public valueH:number=200;
+
+  @Input()
+  public textoBoton:string;
+
+  @Output()
+  public cambioSueldo= new EventEmitter();
+
+  // @Output()
+  // public aumentarTamano= new EventEmitter();
+
+  constructor() {
+    console.log('Instanciando');
+  }// Instancia la clase      NO ES BUENA IDEA PONER COSAS EN EL CONSTRUCTOR
+
+  ngOnInit():void {  //  La clase esta lista
+    console.log('Esta listo');
+    // console.log(this.titulo, this.textoImagen, this.textoBoton);
+
+  }
+
+  ngOnDestroy(): void { // La clase esta destruida
+
   }
 
   saludar(){
     alert("HOLAAAAAAA");
+  }
+
+
+  aumentarTamano(){
+    this.valueH +=1;
+    this.valueW +=1;
+  }
+
+  aumentarSueldo(){
+    // this.textoBoton = (Number(this.textoBoton )+ 1).toString();
+    this.textoBoton = (+this.textoBoton + 1).toString();
+    this.cambioSueldo.emit(this.textoBoton);
+    this.aumentarTamano();
   }
 
 }
