fd8259e7ec6eb07472be72609788438982a355e8
Theo96sc <theo_96.sc@outlook.es>
2020-01-03 18:05:36 -0500

    Clase errores

---
+++ 04 - Ionic/comida/src/app/tab3/tab3.page.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+    usuario = {
+        nombre: '',
+        correo: '',
+        edad: 0,
+        password: '',
+        passwordConfirmacion: '',
+    };
+
+    constructor() {
+    }
+
+
+    crearUsuario(formulario: NgForm) {
+      console.log(formulario.value);
+    }
+
+    validarPasswordsIguales():boolean{
+        if(this.usuario.password === this.usuario.passwordConfirmacion){
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+
+}
